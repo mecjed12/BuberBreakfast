ca00e0dea15c9a4104c05bcb65b31467c3918e56
aleks <exopas@outlook.de>
2023-03-30 19:48:13 +0200

    new project

---
+++ BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,13 @@
+
+
+public record CreateBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
+
+
+
+
